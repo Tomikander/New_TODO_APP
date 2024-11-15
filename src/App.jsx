100101e4f3daa8e943984404056d3aa282de1ba9
Tomikander <andersontomik@gmail.com>
2024-11-15 22:45:11 +0300

    first new branch main New_todo_App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import { FirstPage } from "./components/page/FirstPage";
+
+function App() {
+  return (
+    <FirstPage />
+  )
+};  
+
+export default App;
